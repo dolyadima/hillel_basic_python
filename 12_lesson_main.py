d79515aabc7888cad41a00ef98f19a77a6ac98b7
Dima Dolya <dimchik96@gmail.com>
2021-12-04 12:27:19 +0200

    five commit

--- 12_lesson_main.py
+++ 12_lesson_main.py
@@ -1,5 +1,20 @@
-from functools import reduce
+# from functools import reduce
+#
+# letters = ["P", "Y", "T", "H", "O", "N"]
+# word = reduce(lambda x, y: x + y, letters, 'language')  # TODO: Debug me all please!
+# print(word)
 
-letters = ["P", "Y", "T", "H", "O", "N"]
-word = reduce(lambda x, y: x + y, letters, 'language')  # TODO: Debug me all please!
-print(word)
+
+# class Counter:
+#     def __new__(cls):
+#         obj = super(Counter, cls).__new__(cls)
+#         if not hasattr(cls, 'objects_list'):
+#             cls.objects_list = []
+#         cls.objects_list.append(obj)
+#         return obj
+#
+#
+# c1 = Counter()
+# c2 = Counter()
+# c3 = Counter()
+# print(c1.objects_list)
