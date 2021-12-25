879fbfc5fe48404dcf687e48e9d18263307f14f4
Dima Dolya <dimchik96@gmail.com>
2021-12-25 20:46:29 +0200

    fix for 3.10.1

--- lesson_15/test_match.py
+++ lesson_15/test_match.py
@@ -1,12 +1,12 @@
-def run_action(user_input) -> None:
-    match user_input:
-        case [name, number]:
-            print(f'list')
-        case (name, number):
-            print(f'tuple')
-        case _:
-            print(f'Wrong input')
-
-
-run_action('name 200'.split())
-run_action(('name', '200'))
+# def run_action(user_input) -> None:
+#     match user_input:
+#         case [name, number]:
+#             print(f'list')
+#         case (name, number):
+#             print(f'tuple')
+#         case _:
+#             print(f'Wrong input')
+#
+#
+# run_action('name 100'.split())
+# run_action(('name', '200'))
