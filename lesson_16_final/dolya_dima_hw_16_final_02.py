d44189f44254fddaf54a39ac00a4932bac77fa1a
Dima Dolya <dimchik96@gmail.com>
2021-12-29 22:27:41 +0200

    lesson 16 hw: task 1,4 and tests

--- lesson_16_final/dolya_dima_hw_16_final_02.py
+++ lesson_16_final/dolya_dima_hw_16_final_02.py
@@ -1,4 +1,58 @@
 #
+# TASK 1 v1.0
+#
+from random import randint
+
+
+class Game:
+    __choices: list = ['rock', 'scissors', 'paper', 'lizard', 'spock']
+
+    @classmethod
+    def variants(cls):
+        return ' '.join(cls.__choices)
+
+    @classmethod
+    def rand_choice(cls):
+        return cls.__choices[randint(0, len(cls.__choices)-1)]
+
+    @classmethod
+    def who_win(cls, player_input: str):
+        player_ch: str = player_input
+        pc_choice: str = cls.rand_choice()
+        result: str = f'Player: {player_ch}\nComputer: {pc_choice}\n'
+        if (player_ch == 'rock' and pc_choice in ['scissors', 'lizard']) or\
+           (player_ch == 'scissors' and pc_choice in ['lizard', 'paper']) or\
+           (player_ch == 'paper' and pc_choice in ['rock', 'spock']) or\
+           (player_ch == 'lizard' and pc_choice in ['paper', 'spock']) or\
+           (player_ch == 'spock' and pc_choice in ['rock', 'scissors']):
+            return result + f'Player WIN!'
+        elif player_ch == pc_choice:
+            return result + f'Choices equal. Nobody won.'
+        else:
+            return result + f'Computer WIN!'
+
+    @staticmethod
+    def invalid(param: str):
+        return f'Invalid input "{param}"'
+
+
+# if __name__ == '__main__':
+#     while True:
+#         user_input: str = input(f'Your choice ({Game.variants()})?\n').lower()
+#         if user_input in Game.variants():
+#             print(f'{Game.who_win(user_input)}')
+#             while True:
+#                 repeat_answer: str = input(f'Repeat (Y/N)?\n')
+#                 if repeat_answer in ['Y', 'y']:
+#                     break
+#                 elif repeat_answer in ['N', 'n']:
+#                     exit(0)
+#                 else:
+#                     print(Game.invalid(repeat_answer))
+#         else:
+#             print(Game.invalid(user_input))
+
+#
 # TASK 4 v1.2
 #
 """
@@ -63,37 +117,37 @@ class Exchanger:
         return f'INVALID INPUT\n'
 
 
-if __name__ == '__main__':
-
-    exchanger1 = Exchanger(r'./currency_data_03.txt')
-
-    while True:
-        user_input: str = input(f'COMMAND?\n').upper()
-        command: list = user_input.split()
-
-        if len(command) == 0:
-            print(exchanger1.invalid_input())
-        elif command[0] == 'STOP':
-            print(f'SERVICE STOPPED\n')
-            break
-        elif command[0] == 'COURSE':
-            try:
-                exchanger1.read_data()
-                print(exchanger1.get_currency_info(command[1]))
-            except FileNotFoundError:
-                print('File of currency not found!')
-            except IndexError:
-                print(exchanger1.invalid_input())
-        elif command[0] == 'EXCHANGE':
-            try:
-                exchanger1.read_data()
-                print(exchanger1.make_exchange(command[1], float(command[2])))
-                exchanger1.write_data()
-            except FileNotFoundError:
-                print('File of currency not found!')
-            except IndexError:
-                print(exchanger1.invalid_input())
-            except ValueError:
-                print(exchanger1.invalid_input())
-        else:
-            print(exchanger1.invalid_input())
+# if __name__ == '__main__':
+#
+#     exchanger1 = Exchanger(r'./currency_data_03.txt')
+#
+#     while True:
+#         user_input: str = input(f'COMMAND?\n').upper()
+#         command: list = user_input.split()
+#
+#         if len(command) == 0:
+#             print(exchanger1.invalid_input())
+#         elif command[0] == 'STOP':
+#             print(f'SERVICE STOPPED\n')
+#             break
+#         elif command[0] == 'COURSE':
+#             try:
+#                 exchanger1.read_data()
+#                 print(exchanger1.get_currency_info(command[1]))
+#             except FileNotFoundError:
+#                 print('File of currency not found!')
+#             except IndexError:
+#                 print(exchanger1.invalid_input())
+#         elif command[0] == 'EXCHANGE':
+#             try:
+#                 exchanger1.read_data()
+#                 print(exchanger1.make_exchange(command[1], float(command[2])))
+#                 exchanger1.write_data()
+#             except FileNotFoundError:
+#                 print('File of currency not found!')
+#             except IndexError:
+#                 print(exchanger1.invalid_input())
+#             except ValueError:
+#                 print(exchanger1.invalid_input())
+#         else:
+#             print(exchanger1.invalid_input())
--- lesson_16_final/tests_hw_04.py
+++ lesson_16_final/tests_hw_04.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from .dolya_dima_hw_16_final_02 import Exchanger
+from .dolya_dima_hw_16_final_02 import Exchanger, Game
 
 
 class TestsInput(unittest.TestCase):
@@ -10,20 +10,26 @@ USD,RATE:27.5,AVAILABLE:13500.98
 UAH,RATE:27.3,AVAILABLE:39345.5
     """
 
-    def test_course_uah(self):
+    def test_exch_course_uah(self):
         exchanger2 = Exchanger(os.path.dirname(os.path.realpath(__file__)) + '/currency_data_03.txt')
         exchanger2.read_data()
         self.assertEqual(exchanger2.get_currency_info('UAH'), 'RATE 27.3, AVAILABLE 39345.5\n')
 
-    def test_course_usd(self):
+    def test_exch_course_usd(self):
         exchanger2 = Exchanger(os.path.dirname(os.path.realpath(__file__)) + '/currency_data_03.txt')
         exchanger2.read_data()
         self.assertEqual(exchanger2.get_currency_info('UAH'), 'RATE 27.3, AVAILABLE 39345.5\n')
 
-    def test_invalid_input(self):
+    def test_exch_invalid(self):
         exchanger2 = Exchanger(os.path.dirname(os.path.realpath(__file__)) + '/currency_data_03.txt')
         exchanger2.read_data()
         self.assertEqual(exchanger2.get_currency_info('BTC'), 'INVALID CURRENCY BTC\n')
+
+    def test_game_invalid(self):
+        self.assertEqual(Game.invalid('text'), 'Invalid input "text"')
+
+    def test_game_variants(self):
+        self.assertEqual(Game.variants(), 'rock scissors paper lizard spock')
 
 
 if __name__ == '__main__':
